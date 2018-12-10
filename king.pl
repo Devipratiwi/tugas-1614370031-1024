15b1f0a7c09ec0d2400bd0d1456864adcd7b2da3
Devipratiwi <45458473+Devipratiwi@users.noreply.github.com>
2018-12-10 20:40:22 +0700

    Add files via upload

---
+++ king.pl
@@ -0,0 +1,20 @@
+keturunan(tuti).
+keturunan(rita).
+keturunan(salma).
+keturunan(abdul).
+keturunan(husin).
+
+keturunanlaki(abdul).
+keturunanlaki(husin).
+
+keturunanperempuan(rita).
+keturunanperempuan(salma).
+
+orangtua(salma, arfan).
+orangtua(salma, farhan).
+orangtua(farhan, arfan).
+orangtua(basir, arfan).
+
+nenek(farhan).
+nenek(siti).
+nenek(salsa).
