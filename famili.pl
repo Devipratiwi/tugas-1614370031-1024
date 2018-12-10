620a6f822ad0269223020f6ae64da14ce417c1eb
Devipratiwi <45458473+Devipratiwi@users.noreply.github.com>
2018-12-10 20:41:31 +0700

    Add files via upload

---
+++ famili.pl
@@ -0,0 +1,37 @@
+anak(raja, bahrun).
+anak(raja, farah).
+anak(bahrun, fahri).
+anak(bahrun, salma).
+anak(farah, ferdi). 
+anak(farah, burhan).
+anak(fahri, salsa).
+anak(fahri, arfan).
+anak(salma, fariz).
+anak(ferdi, farhan).
+anak(ferdi, syifa).
+
+male(bahrun).
+male(fahri).
+male(burhan).
+male(ferdi).
+male(arfan). 
+male(farhan).
+male(fariz).
+male(bahrun).
+
+female(farah).
+female(salma).
+female(salsa).
+female(syifa). 
+
+orangtua(bahrun, burhan).
+orangtua(bahrun, farah).
+orangtua(ferdi, bahrun).
+orangtua(bahrun, fariz).
+orangtua(bahrun, raja).
+
+orangtua(salma, syifa).
+orangtua(salsa,farah).
+orangtua(arfan,salsa).
+orangtua(salsa,salma).
+orangtua(farah,syifa)
